1b24bba5f2a431750ec0b2c559212e453896e9ed
Richard-Schadeberg <Richard.Schadeberg@student.uts.edu.au>
2021-12-17 12:47:56 +1100

    Initial Commit

---
+++ Assets/Scripts/Game/GameTypes.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct CellState {
+    public bool Water;
+    public int contents;
+    public int elevation;
+    public Wetness wetness;
+    public Shadiness shadiness;
+}
+[System.Serializable]
+public enum Wetness {
+    Dry,
+    Damp,
+    Wet
+}
+[System.Serializable]
+public enum Shadiness {
+    Sunny,
+    Shaded,
+    Dark
+}
---
+++ Assets/Scripts/HexGrid/HexGrid.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class HexGrid : MonoBehaviour {
+
+	public int width = 6;
+	public int height = 6;
+
+	public HexCell cellPrefab;
+    HexMesh hexMesh;
+    HexCell[] cells;
+	Canvas gridCanvas;
+	public Color defaultColor = Color.white;
+	public Color touchedColor = Color.magenta;
+	void Awake () {
+		gridCanvas = GetComponentInChildren<Canvas>();
+		hexMesh = GetComponentInChildren<HexMesh>();
+		gridCanvas = GetComponentInChildren<Canvas>();
+		cells = new HexCell[height * width];
+
+		for (int z = 0, i = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
+				CreateCell(x, z, i++);
+			}
+		}
+	}
+    void Start () {
+		hexMesh.Triangulate(cells);
+		moveCamera();
+	}	
+	public void ColorCell (Vector3 position, Color color) {
+		position = transform.InverseTransformPoint(position);
+		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+		int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+		HexCell cell = cells[index];
+		cell.color = color;
+		hexMesh.Triangulate(cells);
+	}
+	void CreateCell (int x, int z, int i) {
+		Vector3 position;	
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+		position.y = 0f;
+		position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+		cell.color = defaultColor;
+
+		if (x > 0) {
+			cell.SetNeighbor(HexDirection.W, cells[i - 1]);
+		}
+		if (z > 0) {
+			if ((z & 1) == 0) {
+				cell.SetNeighbor(HexDirection.SE, cells[i - width]);
+				if (x > 0) {
+					cell.SetNeighbor(HexDirection.SW, cells[i - width - 1]);
+				}
+			}
+			else {
+				cell.SetNeighbor(HexDirection.SW, cells[i - width]);
+				if (x < width - 1) {
+					cell.SetNeighbor(HexDirection.SE, cells[i - width + 1]);
+				}
+			}
+		}
+		Text label = Instantiate<Text>(cellLabelPrefab);
+		label.rectTransform.SetParent(gridCanvas.transform, false);
+		label.rectTransform.anchoredPosition =
+			new Vector2(position.x, position.z);
+		label.text = cell.coordinates.ToStringOnSeparateLines();
+	}
+	public Text cellLabelPrefab;
+	public Camera controlledCamera;
+	public float controlledCameraTilt = 15;
+	void moveCamera() {
+		float fov = controlledCamera.fieldOfView;
+		float bottomAngle = 90 - (fov/2) + controlledCameraTilt;
+		float topAngle    = 90 + (fov/2) + controlledCameraTilt;
+		float bottomSlope = (float)Math.Tan(bottomAngle * Math.PI/180);
+		float topSlope    = (float)Math.Tan(topAngle    * Math.PI/180);
+		float camZ = hexgridHeight() * topSlope / (topSlope - bottomSlope);
+		float camY = camZ * bottomSlope;
+		camZ += hexGridBottom();
+		float camX = hexGridLeft() + (hexGridWidth()/2);
+		Vector3 cameraVector = new Vector3(camX,camY,camZ);
+		controlledCamera.transform.position = transform.position + cameraVector;
+		controlledCamera.transform.rotation = Quaternion.Euler(90-controlledCameraTilt,0,0);
+	}
+	float hexgridHeight() {
+		float cellHeight = 2f * HexMetrics.outerRadius;
+		float overlap = 0.5f * HexMetrics.outerRadius;
+		return height * (cellHeight - overlap) + overlap;
+	}
+	float hexGridWidth() {
+		float cellWidth = 2f * HexMetrics.innerRadius;
+		float overlap = HexMetrics.innerRadius;
+		return width * cellWidth + overlap;
+	}
+	float hexGridBottom() {
+		return -HexMetrics.outerRadius;
+	}
+	float hexGridLeft() {
+		return -HexMetrics.innerRadius;
+	}
+}
